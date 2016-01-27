b059158ed345793b7b28f338dfa6b5d78c1ad265
guipsamora <gui.psamora@gmail.com>
2016-01-27 14:39:41 -0200

    channels objects working

--- script.js
+++ script.js
@@ -2,90 +2,62 @@ var channels = ["freecodecamp","esl_sc2","terakilobyte","habathcx","RobotCaleb",
 
 var channelInfos = [];
 
-var on = [];
-var off = [];
-var non_exist = [];
+function callApi(){ // function that calls the API
 
-var info = {name: "", status: ""};
+    channels.forEach(function(channels){
+      var info = {}; //stores data from each channel
+      // first API call, gives data about the channel, such as game, img, url
+      $.getJSON('https://api.twitch.tv/kraken/channels/' + channels +'?callback=?', function(channel) {
 
-function success(){
-      // function that calls the API
-      for (var i = 0; i < channels.length; i++) {
-            var info = {};
-            //API call that gives me the infos about the channels, such as images, url and so on
-            $.ajax({
-              url: "https://api.twitch.tv/kraken/channels/" + channels[i],
-              data: "json",
-              dataType: 'jsonp',
-              type: 'GET',
-              headers: { 'Api-User-Agent': 'Example/1.0' },
-              success: function(channel) {
-                info.name = channel.name
-                // if(channel.name )
-                  // $("#flex-container").append("<div class=\"flex-item\"></div></a>");
-               // console.log(channel)
-               // logo = channel.logo
-               // nome posso puxar da array lá de cima
-               // link = channel.url
-               // nome do jogo = channel.game
-               // programa = channel.status
-               console.log(info.name)
-
-              }
-            });
-            // API call that tells me if the channels is online, offline or doesn't existe at all
-            $.ajax({
-              url: "https://api.twitch.tv/kraken/streams/" + channels[i],
-              data: "json",
-              dataType: 'jsonp',
-              type: 'GET',
-              headers: { 'Api-User-Agent': 'Example/1.0' },
-              success: function(channel) {
-                  var channelName;
+          if (channel["status"] === 422){
+            var myRegex = /'(.*?)'/g
+            var message = channel["message"]
+            channelName = myRegex.exec(message);
+            info.name = channelName[1];
+          } else {
+            info.name = channel.name;
+            info.url = channel.url;
+            info.img = channel.logo;
+            info.game = channel.game;
+          }
+          // seconde API call, gives data if the channel is live or not or if it doesn't exist at all
+          $.getJSON('https://api.twitch.tv/kraken/streams/' + channels +'?callback=?', function(channel) {
                   if (channel["status"] === 422){
                         var myRegex = /'(.*?)'/g
                         var message = channel["message"]
                         channelName = myRegex.exec(message);
-                        non_exist.push(channelName[1]);
                         info.status = "Account Closed"
-                        // $("#flex-container").append("<div class=\"flex-item closed \"></div></a>");
-                        // $(".closed").append("<p class=\"extract\">Account Closed</p>");
-                 } else if (channel["stream"] == null) {
+                 } else if (channel["stream"] === null) {
                         channelName = channel["_links"].channel.split("/").pop();
-                        off.push(channelName);
                         info.status = "Off"
-                        // $("#flex-container").append("<div class=\"flex-item offline \"></div>");
-                        // $(".offline").append("<p class=\"extract\">" + channelName + "</p>");
-                  } else if ( channel["stream"] != null ) {
+                  } else if ( channel["stream"] !== null ) {
                         channelName = channel["_links"].channel.split("/").pop();
-                        on.push(channelName)
                         info.status = "On"
-                        // $("#flex-container").append("<div class=\"flex-item resultbox online \"></div></a>");
-                        // $(".online").append("<p class=\"extract\">" + channelName + "</p>");
                   }
-               // console.log(info.status)
-              }
-            });
-
-            
-         // $("#flex-container").append("<a target=\"_blank\" href=\" data.streams[i].channel.url \"\"><div class=\"flex-item resultbox" + i + "\"></div></a>");
-         // $(".resultbox" + i + "").append("<p class=\"extract\">" + on[i] + "</p>");
-         console.log(info);
-          // channelInfos.push(info);
-          // console.log(channelInfos);
-      }
-
-      // for (var i = 0; i < channels.length; i++) {
-      //    $("#flex-container").append("<a target=\"_blank\" href=\" data.streams[i].channel.url \"\"><div class=\"flex-item resultbox" + i + "\"></div></a>");
-      //    $(".resultbox" + i + "").append("<p class=\"extract\">" + on[i] + "</p>");
-      // }
-
+                  channelInfos.push(info);
+                  console.log(channelInfos)
+          });
+      });
+    })
 
 }
 
+// 
+// function populateHTML(){
+
+//         // for (var i = 0; i < channels.length; i++) {
+//       //    $("#flex-container").append("<a target=\"_blank\" href=\" data.streams[i].channel.url \"\"><div class=\"flex-item resultbox" + i + "\"></div></a>");
+//       //    $(".resultbox" + i + "").append("<p class=\"extract\">" + on[i] + "</p>");
+//       // }
+
+//       // $("#flex-container").append("<div class=\"flex-item resultbox online \"></div></a>");
+//                         // $(".online").append("<p class=\"extract\">" + channelName + "</p>");
+
+// }
+
 
 $(document).ready(function(){
-   success();
-    console.log(channelInfos);
- 
-});   
+   callApi();
+
+
+});
